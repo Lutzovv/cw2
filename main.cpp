7b4500182bd7b194143f3f52051f1079a9864331
Lutzoww <lutzoww51@gmail.com>
2024-10-09 16:43:08 +0500

    commit

--- main.cpp
+++ main.cpp
@@ -1,58 +1,54 @@
 #include <iostream>
+#include <cstdlib>
 
-double Addition(double num1, double num2) {
-	return num1 + num2;
+void SetRandom(int arr[], const int size) {
+	for (int i = 0; i < size; i++) {
+		arr[i] = rand() % 27 - 8;
+	}
 }
 
-double Subtraction(double num1, double num2) {
-	return num1 - num2;
+void PrintArr(int arr[], const int size) {
+	for (int i = 0; i < size; i++) {
+		std::cout << arr[i] << " ";
+	}
+	std::cout << "\n";
 }
 
-double Multiplication(double num1, double num2) {
-	return num1 * num2;
+void FindMin(int arr[], const int size) {
+	int min_value = arr[0];
+	int min_index;
+
+	for (int i = 0; i < size; i++) {
+		if (min_value > arr[i]) {
+			min_value = arr[i];
+			min_index = i;
+		}
+	}
+	std::cout << "Min number: " << min_value << "\nMin index: " << min_index << "\n";
 }
 
-double Division(double num1, double num2) {
-	return num1 / num2;
-}
+void FindMax(int arr[], const int size) {
+	int max_value = arr[0];
+	int max_index;
 
-double Percent(double num1, double num2) {
-	return (num1 * num2) / 100;
+	for (int i = 0; i < size; i++) {
+		if (max_value < arr[i]) {
+			max_value = arr[i];
+			max_index = i;
+		}
+	}
+	std::cout << "Max number: " << max_value << "\nMax index: " << max_index << "\n";
 }
 
 int main() {
+	srand(time(NULL));
 
-	double num1, num2, result;
-	char action;
+	const int size = 20;
+	int arr[size];
 
-	std::cout << "Enter first number: ";
-	std::cin >> num1;
+	SetRandom(arr, size);
+	PrintArr(arr, size);
+	FindMin(arr, size);
+	FindMax(arr, size);
 
-	std::cout << "Enter action (+, -, /, *, %): ";
-	std::cin >> action;
-
-	do {
-		std::cout << "Enter second number: ";
-		std::cin >> num2;
-	} while (action == '/' && num2 == 0);
-
-	if (action == '+') {
-		result = Addition(num1, num2);
-	}
-	else if (action == '-') {
-		result = Subtraction(num1, num2);
-	}
-	else if (action == '*') {
-		result = Multiplication(num1, num2);
-	}
-	else if (action == '/') {
-		result = Division(num1, num2);
-	}
-	else if (action == '%') {
-		result = Percent(num1, num2);
-	}
-
-	std::cout << num1 << " " << action << " " << num2 << " = " << result;
-
-	return 0;
 }
